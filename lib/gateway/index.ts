71ac0264a2ce0bb997fa306a61982ad6e7a42e28
BlackusPL <45206560+BlackusPL@users.noreply.github.com>
2023-06-30 22:10:43 +0200

    remove all and again upload

--- lib/gateway/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Client";
-export * from "./structures/ClientUser";
-export * from "./structures/Transport";
-export * from "./structures/Util"
-export * from "./constants/Constants"
-export * from "./constants/Payload"
-export * from "./transport/WebsocketTransport"
