928686783b12cfd65ba0155efec86c75ea3ec4af
BlackusPL <45206560+BlackusPL@users.noreply.github.com>
2023-06-30 22:10:57 +0200

    Added it back

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Kizzy Rpc Improved",
+  description:
+    "Rpc on The Web",
+  mainNav: [],
+  links: {
+    github: "https://github.com/dead8309",
+    discord: 'https://discord.gg/vUPc7zzpV5',
+    app: 'https://github.com/dead8309/Kizzy'
+  },
+}
